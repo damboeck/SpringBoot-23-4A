2f279bc495b190d1a26789a54f44306c17d0b2a5
Werner <werner.damboeck@letto.at>
2023-04-25 08:50:48 +0200

    Initial Commit

---
+++ src/main/java/at/ac/htlstp/k4a23/springboot234a/SpringBoot234AApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.k4a23.springboot234a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot234AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot234AApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/htlstp/k4a23/springboot234a/SpringBoot234AApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.k4a23.springboot234a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot234AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
